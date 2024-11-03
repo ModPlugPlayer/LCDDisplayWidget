5c8e56a31770328092a4982ff02b00fe13db0108
Volkan Orhan <contactme@volkanorhan.com>
2024-11-03 18:22:44 +0300

    Initial commit

---
+++ test/ControlWindow.cpp
@@ -0,0 +1,219 @@
+#include "ControlWindow.hpp"
+#include "ui_ControlWindow.h"
+#include <PlayList.hpp>
+
+ControlWindow::ControlWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ControlWindow)
+{
+    ui->setupUi(this);
+    QObject::connect(this, &ControlWindow::changeRepeat, this, &ControlWindow::onChangeRepeat);
+    QObject::connect(this, qOverload<>(&ControlWindow::play), this, qOverload<>(&ControlWindow::onPlay));
+    QObject::connect(this, qOverload<>(&ControlWindow::pause), this, qOverload<>(&ControlWindow::onPause));
+    QObject::connect(this, qOverload<>(&ControlWindow::resume), this, qOverload<>(&ControlWindow::onResume));
+    QObject::connect(this, qOverload<>(&ControlWindow::stop), this, qOverload<>(&ControlWindow::onStop));
+    QObject::connect(this, &ControlWindow::previous, this, &ControlWindow::onPrevious);
+    QObject::connect(this, &ControlWindow::next, this, &ControlWindow::onNext);
+    ui->playlistButton->click();
+}
+
+/*
+ControlWindow::ControlWindow(PlayListWindow * playListWindow) :
+    QDialog(playListWindow),
+    ui(new Ui::ControlWindow)
+{
+    this->playListWindow = playListWindow;
+    ui->setupUi(this);
+}*/
+
+ControlWindow::~ControlWindow() {
+    delete ui;
+}
+
+int ControlWindow::getVolume() const {
+    return 0;
+}
+
+bool ControlWindow::isAlwaysOnTop() const {
+    return false;
+}
+
+bool ControlWindow::isTitleBarHidden() const {
+    return false;
+}
+
+bool ControlWindow::isSnapToViewPort() const {
+    return false;
+}
+
+bool ControlWindow::isKeptStayingInViewPort() const {
+    return false;
+}
+
+void ControlWindow::onOpen(std::filesystem::path filePath) {
+    playingMode = PlayingMode::SingleTrack;
+}
+
+void ControlWindow::onOpen(PlayListItem playListItem) {
+    playingMode = PlayingMode::PlayList;
+    ui->display->setSongTitle(playListItem.filePath.filename().c_str());
+    emit open(playListItem);
+}
+
+void ControlWindow::onStop() {
+    playingStatus = PlayingStatus::Stopped;
+    ui->stopButton->setStyleSheet("color: green;");
+    ui->pauseButton->setStyleSheet("color: blue;");
+    ui->playButton->setStyleSheet("color: blue;");
+}
+
+void ControlWindow::onStop(const PlayListItem playListItem) {
+    playingStatus = PlayingStatus::Stopped;
+    ui->stopButton->setStyleSheet("color: green;");
+    ui->pauseButton->setStyleSheet("color: blue;");
+    ui->playButton->setStyleSheet("color: blue;");
+}
+
+void ControlWindow::onPlay() {
+    playingStatus = PlayingStatus::Playing;
+    ui->stopButton->setStyleSheet("color: blue;");
+    ui->pauseButton->setStyleSheet("color: blue;");
+    ui->playButton->setStyleSheet("color: green;");
+}
+
+void ControlWindow::onPlay(const PlayListItem playListItem) {
+    playingStatus = PlayingStatus::Playing;
+    ui->stopButton->setStyleSheet("color: blue;");
+    ui->pauseButton->setStyleSheet("color: blue;");
+    ui->playButton->setStyleSheet("color: green;");
+}
+
+void ControlWindow::onPause() {
+    playingStatus = PlayingStatus::Paused;
+    ui->stopButton->setStyleSheet("color: blue;");
+    ui->pauseButton->setStyleSheet("color: green;");
+    ui->playButton->setStyleSheet("color: blue;");
+}
+
+void ControlWindow::onPause(const PlayListItem playListItem) {
+    playingStatus = PlayingStatus::Paused;
+    ui->stopButton->setStyleSheet("color: blue;");
+    ui->pauseButton->setStyleSheet("color: green;");
+    ui->playButton->setStyleSheet("color: blue;");
+}
+
+void ControlWindow::onResume() {
+    playingStatus = PlayingStatus::Playing;
+    ui->stopButton->setStyleSheet("color: blue;");
+    ui->pauseButton->setStyleSheet("color: blue;");
+    ui->playButton->setStyleSheet("color: green;");
+}
+
+void ControlWindow::onResume(const PlayListItem playListItem) {
+    playingStatus = PlayingStatus::Playing;
+    ui->stopButton->setStyleSheet("color: blue;");
+    ui->pauseButton->setStyleSheet("color: blue;");
+    ui->playButton->setStyleSheet("color: green;");
+}
+
+void ControlWindow::onPrevious() {
+}
+
+void ControlWindow::onNext() {
+}
+
+void ControlWindow::onChangeVolume(int volume) {
+
+}
+
+void ControlWindow::onScrubTime(int position) {
+
+}
+
+void ControlWindow::onChangeRepeat(RepeatState repeat) {
+    repeatState = repeat;
+}
+
+void ControlWindow::onSetAlwaysOnTop(bool alwaysOnTop) {
+
+}
+
+void ControlWindow::onHideTitleBar(bool hide) {
+
+}
+
+void ControlWindow::onSetSnapToViewPort(bool snapToViewPort) {
+
+}
+
+void ControlWindow::onSetKeepStayingInViewPort(bool keepStayingInViewPort)
+{
+
+}
+
+void ControlWindow::onSetSnappingThreshold(int snappingThreshold)
+{
+
+}
+
+void ControlWindow::on_previousButton_clicked() {
+    emit previous();
+}
+
+void ControlWindow::on_nextButton_clicked() {
+    emit next();
+}
+
+void ControlWindow::on_playButton_clicked() {
+    emit play();
+}
+
+void ControlWindow::on_pauseButton_clicked() {
+    if(playingStatus == PlayingStatus::Playing)
+        emit pause();
+    else if(playingStatus == PlayingStatus::Paused)
+        emit resume();
+}
+
+void ControlWindow::on_stopButton_clicked() {
+    emit stop();
+}
+
+void ControlWindow::on_repeatButton_clicked() {
+    toggleRepeat();
+    if(repeatState == RepeatState::None)
+        ui->repeatButton->setText("Repeat\nNone");
+    if(repeatState == RepeatState::SingleTrack)
+        ui->repeatButton->setText("Repeat\nSong");
+    if(repeatState == RepeatState::PlayList)
+        ui->repeatButton->setText("Repeat\nPlaylist");
+}
+
+void ControlWindow::on_playlistButton_clicked() {
+    //ui->actionPlayListEditor->setChecked(turnOn);
+    //ui->optionButtons->togglePlayListEditorButton(turnOn);
+}
+
+void ControlWindow::onPlayListEditorIsHidden() {
+    ui->playlistButton->setStyleSheet("color: blue;");
+}
+
+void ControlWindow::toggleRepeat() {
+    RepeatState repeat = RepeatState::None;
+
+    if(repeatState == RepeatState::None)
+        repeat = RepeatState::SingleTrack;
+    else
+        if(repeatState == RepeatState::SingleTrack)
+            repeat = RepeatState::PlayList;
+    else
+        if(repeatState == RepeatState::PlayList)
+            repeat = RepeatState::None;
+
+    emit changeRepeat(repeat);
+}
+
+void ControlWindow::on_openButton_clicked() {
+
+}
+
---
+++ test/ControlWindow.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <QDialog>
+#include <Player.hpp>
+
+namespace Ui {
+class ControlWindow;
+}
+
+using namespace ModPlugPlayer;
+
+class ControlWindow : public QDialog, public ModPlugPlayer::Player
+{
+    Q_OBJECT
+
+public:
+    explicit ControlWindow(QWidget *parent = nullptr);
+    //explicit ControlWindow(PlayListWindow *playListWindow = nullptr);
+    ~ControlWindow();
+    int getVolume() const override;
+    bool isAlwaysOnTop() const override;
+    bool isTitleBarHidden() const override;
+    bool isSnapToViewPort() const override;
+    bool isKeptStayingInViewPort() const override;
+
+signals:
+    void stop() override;
+    void stop(const ModPlugPlayer::PlayListItem playListItem) override;
+    void play() override;
+    void play(const ModPlugPlayer::PlayListItem playListItem) override;
+    void pause() override;
+    void pause(const ModPlugPlayer::PlayListItem playListItem) override;
+    void resume() override;
+    void resume(const ModPlugPlayer::PlayListItem playListItem) override;
+    void previous() override;
+    void next() override;
+    void changeVolume(const int volume) override;
+    void changeRepeat(const ModPlugPlayer::RepeatState repeat) override;
+    void setAlwaysOnTop(const bool alwaysOnTop) override;
+    void hideTitleBar(const bool hide) override;
+    void snapToViewPort(const bool toBeSnappedToViewPort) override;
+    void keepStayingInViewPort(const bool toBeKeptStayingInViewPort) override;
+    void open(const std::filesystem::path filePath) override;
+    void open(const ModPlugPlayer::PlayListItem playListItem) override;
+    void scrubTime(const int position) override;
+
+public slots:
+    void onOpen(const std::filesystem::path filePath) override;
+    void onOpen(const ModPlugPlayer::PlayListItem playListItem) override;
+    void onStop() override;
+    void onStop(const ModPlugPlayer::PlayListItem playListItem) override;
+    void onPlay() override;
+    void onPlay(const ModPlugPlayer::PlayListItem playListItem) override;
+    void onPause() override;
+    void onPause(const ModPlugPlayer::PlayListItem playListItem) override;
+    void onResume() override;
+    void onResume(const ModPlugPlayer::PlayListItem playListItem) override;
+    void onPrevious() override;
+    void onNext() override;
+    void onChangeVolume(const int volume) override;
+    void onScrubTime(const int position) override;
+    void onChangeRepeat(const ModPlugPlayer::RepeatState repeat) override;
+    void onSetAlwaysOnTop(const bool alwaysOnTop) override;
+    void onHideTitleBar(const bool hide) override;
+    void onSetSnapToViewPort(const bool snapToViewPort) override;
+    void onSetKeepStayingInViewPort(const bool keepStayingInViewPort) override;
+    void onSetSnappingThreshold(const int snappingThreshold) override;
+
+private slots:
+    void on_previousButton_clicked();
+    void on_nextButton_clicked();
+    void on_playButton_clicked();
+    void on_pauseButton_clicked();
+    void on_stopButton_clicked();
+    void on_repeatButton_clicked();
+    void on_playlistButton_clicked();
+    void onPlayListEditorIsHidden();
+
+    void on_openButton_clicked();
+
+private:
+    Ui::ControlWindow *ui;
+    RepeatState repeatState = RepeatState::None;
+    PlayingStatus playingStatus = PlayingStatus::Stopped;
+    void toggleRepeat();
+    PlayingMode playingMode = PlayingMode::SingleTrack;
+};
+
