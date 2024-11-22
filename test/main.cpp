7aecd7c728c51cadb3ba8e56909fd99af77165c4
Volkan Orhan <contactme@volkanorhan.com>
2024-11-22 10:15:48 +0300

    License comments were added to class definiton and declaration files

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
 /*
-MainWindow function definition for the test purposes of PlayListWidget
-Copyright (C) 2022 Volkan Orhan
+Main function definition for the test purposes of LCDDisplayWidget
+Copyright (C) 2024 Volkan Orhan
 
 This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
 
