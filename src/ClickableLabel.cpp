fd9633958f712f61e65c432b2b10da3484f14d29
Volkan Orhan <contactme@volkanorhan.com>
2024-12-02 10:12:24 +0300

    Missing ClickableLabel class was added

---
+++ src/ClickableLabel.cpp
@@ -0,0 +1,20 @@
+#include "ClickableLabel.hpp"
+#include <QMouseEvent>
+
+ClickableLabel::ClickableLabel(QWidget* parent, Qt::WindowFlags f)
+    : QLabel(parent) {
+}
+
+ClickableLabel::~ClickableLabel() {}
+
+void ClickableLabel::mousePressEvent(QMouseEvent* event) {
+}
+
+void ClickableLabel::mouseDoubleClickEvent(QMouseEvent *event) {
+    emit doubleClicked();
+}
+
+void ClickableLabel::mouseReleaseEvent(QMouseEvent *event) {
+    emit clicked();
+}
+
